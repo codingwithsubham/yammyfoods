0d5f9e55b7ea49a3368390163fd55ccfbc2769b5
Subham Pal <subhamp@infinite.com>
2020-10-14 11:27:45 +0530

    production code

--- server.js
+++ server.js
@@ -13,11 +13,13 @@ app.use('/api/cart', require('./routes/api/cart'));
 app.use('/api/category', require('./routes/api/category'));
 
 // Serve static assets in production
-// Set static folder
-app.use(express.static('client/build'));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5433;
 
