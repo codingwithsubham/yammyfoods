8f225bc01971dc74a7de83c3f22baa75cf3b94bc
Subham Pal <magicianpaul88@gmail.com>
2021-01-12 18:10:11 +0530

    slider changes

--- client/src/components/dashboard/LatestProductSlider.js
+++ client/src/components/dashboard/LatestProductSlider.js
@@ -13,19 +13,45 @@ const LatestProductSlider = ({
 
   let slider = [1, 2, 3, 4, 5];
 
+  var slideIndex = 0;
+  showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  if(slides[slideIndex-1]){
+  slides[slideIndex-1].style.display = "block";  
+  }
+  if( dots[slideIndex-1]){
+  dots[slideIndex-1].className += " active-slide";
+  }
+  setTimeout(showSlides, 6000); // Change image every 2 seconds
+}
+
   return (
     products &&
     (loading ? (
       <div className='dmy-slider'></div>
     ) : (
       <Fragment>
-        <div className='img-slider'>
+        <div className="slideshow-container">
           {slider.map((item, idx) => (
+            <div key={idx} className="mySlides slide">  
             <img
-              key={idx}
               alt=''
               src={require(`../../sliders/abb${item}.jpg`)}
+              style={{width: '100%'}}
             />
+            </div>
           ))}
         </div>
       </Fragment>
