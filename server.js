20230a7408a7e399e45c0c35231599127b6a3b93
Subham Pal <magicianpaul88@gmail.com>
2021-05-28 21:01:29 +0530

    server issue resolved

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const path = require('path');
-const connectDB = require('./config/db');
+const express = require("express");
+const path = require("path");
+const connectDB = require("./config/db");
 const app = express();
 
 // Connect Database
@@ -8,23 +8,20 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 // Define Routes
-app.use('/api/products', require('./routes/api/products'));
-app.use('/api/cart', require('./routes/api/cart'));
-app.use('/api/category', require('./routes/api/category'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/checkout', require('./routes/api/checkout'));
-app.use('/api/orders', require('./routes/api/order'));
-app.use('/api/wallet', require('./routes/api/wallet'));
-app.use('/api/razorpay', require('./routes/api/razorpay'));
+app.use("/api/products", require("./routes/api/products"));
+app.use("/api/cart", require("./routes/api/cart"));
+app.use("/api/category", require("./routes/api/category"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/checkout", require("./routes/api/checkout"));
+app.use("/api/orders", require("./routes/api/order"));
+app.use("/api/wallet", require("./routes/api/wallet"));
+app.use("/api/razorpay", require("./routes/api/razorpay"));
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+// Set static folder
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5433;
 
